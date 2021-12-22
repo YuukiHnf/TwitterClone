13b489c40d549540edab5fe3d47c3d5ba113a1d8
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2021-12-22 14:04:27 +0900

    firebase/auth/onAuthStateChangedでobserverを作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from "react";
 import styples from "./App.module.css";
 
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { login, logout } from "./features/userSlice";
+import { auth } from "./firebase/firebase";
+
 const App: React.VFC = () => {
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (users) => {
+      if (users) {
+        dispatch(
+          login({
+            uid: users.uid,
+            photoUrl: users.photoURL ?? "",
+            displayName: users.displayName ?? "",
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+  }, []);
+
   return <div className="App"></div>;
 };
 
--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootStateOrAny } from "react-redux";
 
-const initialState = {
+const initialState: { user: UserType } = {
   user: { uid: "", photoUrl: "", displayName: "" },
+};
+
+type UserType = {
+  uid: string;
+  photoUrl: string;
+  displayName: string;
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    login: (state, action) => {
+    login: (state, action: { payload: UserType }) => {
       state.user = action.payload;
     },
     logout: (state, action) => {
