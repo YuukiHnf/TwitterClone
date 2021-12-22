353a78f0e9b355503f2a853078e44fa0eed15cc2
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2021-12-22 16:08:51 +0900

    LogoutでAuthとuser情報を初期化した

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,29 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAppDispatch } from "../app/hooks";
+import { logout } from "../features/userSlice";
+import { auth } from "../firebase/firebase";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const dispatch = useAppDispatch();
+
+  const onSignOut = async () => {
+    console.log("try:signOut");
+    try {
+      await signOut(auth);
+      dispatch(logout({}));
+      console.log("success:signOut");
+    } catch (e: any) {
+      alert(e.message);
+    }
+  };
+
+  return (
+    <div>
+      Feed
+      <button onClick={onSignOut}>SignOut</button>
+    </div>
+  );
 };
 
 export default Feed;
