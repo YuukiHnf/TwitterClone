45fbade242d78cf81c9a3240daf06b2e0b93af45
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2021-12-24 15:56:36 +0900

    subCollectionを作ってDBにリプライを入れた

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,12 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { useAppSelector } from "../app/hooks";
 import { selectUser } from "../features/userSlice";
 import { TweetType } from "./TweetInput";
 import styles from "./Post.module.css";
+import SendIcon from "@material-ui/icons/Send";
+import { addDoc, collection, doc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 type PostType = {
   post: TweetType;
@@ -11,7 +14,26 @@ type PostType = {
 
 const Post: React.VFC<PostType> = (props: PostType) => {
   const { post } = props;
-  const users = useAppSelector(selectUser);
+  const user = useAppSelector(selectUser);
+
+  const [comment, setComment] = useState("");
+  const onNewComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // この投稿のDocをとってくる
+    const ptrPostDoc = doc(collection(db, "posts"), post.id);
+
+    // この投稿のDocのSubCollectionのDocをとってくる
+    const subCollection = collection(ptrPostDoc, "comments");
+
+    // コレクションから、データを追加する
+    const docRef = await addDoc(subCollection, {
+      avatar: user.photoUrl,
+      text: comment,
+      timeStamp: serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
 
   return (
     <div className={styles.post}>
@@ -38,6 +60,26 @@ const Post: React.VFC<PostType> = (props: PostType) => {
           <img src={post.image} alt="tweet" />
         </div>
       )}
+      <form onSubmit={onNewComment}>
+        <div className={styles.post_form}>
+          <input
+            className={styles.post_input}
+            type="text"
+            placeholder="Type new comment..."
+            value={comment}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setComment(e.target.value);
+            }}
+          />
+          <button
+            disabled={!comment}
+            className={comment ? styles.post_button : styles.post_buttonDisable}
+            type="submit"
+          >
+            <SendIcon className={styles.post_sendIcon} />
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
