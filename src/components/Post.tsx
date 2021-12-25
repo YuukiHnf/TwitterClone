d8deefc28841e193bb1dcd7baee06a129e43ba02
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2021-12-25 14:24:26 +0900

    replayを表示させて、Buttonで表示非表示を決めた

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, makeStyles } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../app/hooks";
 import { selectUser } from "../features/userSlice";
@@ -16,6 +16,7 @@ import {
   Timestamp,
 } from "firebase/firestore";
 import { db } from "../firebase/firebase";
+import MessageIcon from "@material-ui/icons/Send";
 
 type PostType = {
   post: TweetType;
@@ -29,12 +30,23 @@ type ReplayType = {
   username: string;
 };
 
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
+
 const Post: React.VFC<PostType> = (props: PostType) => {
   const { post } = props;
   const user = useAppSelector(selectUser);
+  const classes = useStyles();
 
   const [comment, setComment] = useState("");
   const [replays, setReplays] = useState<Array<ReplayType>>([]);
+
+  const [openReplay, setOpenReplay] = useState(false);
 
   useEffect(() => {
     // この投稿のreplayのCollection
@@ -62,7 +74,7 @@ const Post: React.VFC<PostType> = (props: PostType) => {
     };
   }, [post.id]);
 
-  console.log(`${post.text} : reps are ${replays}`);
+  //   console.log(`${post.text} : reps are ${replays}`);
 
   const onNewComment = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -84,7 +96,7 @@ const Post: React.VFC<PostType> = (props: PostType) => {
 
   return (
     <div className={styles.post}>
-      <div className={styles.post_avatart}>
+      <div className={styles.post_avatar}>
         <Avatar src={post.avatar} />
       </div>
       <div className={styles.post_body}>
@@ -101,32 +113,55 @@ const Post: React.VFC<PostType> = (props: PostType) => {
             <p>{post.text}</p>
           </div>
         </div>
+        {post.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={post.image} alt="tweet" />
+          </div>
+        )}
+        <h4
+          onClick={() => setOpenReplay(!openReplay)}
+          style={{ color: "#00bfff" }}
+        >
+          Replay...
+        </h4>
+        {openReplay && (
+          <>
+            {replays.map((com) => (
+              <div key={com.id} className={styles.post_comment}>
+                <Avatar src={com.avatar} className={classes.small} />
+
+                <span className={styles.post_commentUser}>@{com.username}</span>
+                <span className={styles.post_commentText}>{com.text} </span>
+                <span className={styles.post_headerTime}>
+                  {new Date(com.timeStamp?.toDate()).toLocaleString()}
+                </span>
+              </div>
+            ))}
+            <form onSubmit={onNewComment}>
+              <div className={styles.post_form}>
+                <input
+                  className={styles.post_input}
+                  type="text"
+                  placeholder="Type new comment..."
+                  value={comment}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setComment(e.target.value);
+                  }}
+                />
+                <button
+                  disabled={!comment}
+                  className={
+                    comment ? styles.post_button : styles.post_buttonDisable
+                  }
+                  type="submit"
+                >
+                  <SendIcon className={styles.post_sendIcon} />
+                </button>
+              </div>
+            </form>
+          </>
+        )}
       </div>
-      {post.image && (
-        <div className={styles.post_tweetImage}>
-          <img src={post.image} alt="tweet" />
-        </div>
-      )}
-      <form onSubmit={onNewComment}>
-        <div className={styles.post_form}>
-          <input
-            className={styles.post_input}
-            type="text"
-            placeholder="Type new comment..."
-            value={comment}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setComment(e.target.value);
-            }}
-          />
-          <button
-            disabled={!comment}
-            className={comment ? styles.post_button : styles.post_buttonDisable}
-            type="submit"
-          >
-            <SendIcon className={styles.post_sendIcon} />
-          </button>
-        </div>
-      </form>
     </div>
   );
 };
